0de1d1c5977ba4e749c23f90c09a855ae333fb3a
Ismael Aceves <ismael@Ismaels-MacBook-Pro.local>
2022-10-27 01:14:49 -0400

    Add reviews

--- game-review-client/src/App.js
+++ game-review-client/src/App.js
@@ -7,47 +7,43 @@ import { useEffect, useState } from "react";
 import GameReview from "./components/GameReview";
 import Login from "./components/Login";
 
-
 const App = () => {
   const [games, setGames] = useState([]);
   const [id, setId] = useState(0);
-  const [userLoggedIn, setUserLoggedIn] = useState(false);
+  const [userLoggedIn, setUserLoggedIn] = useState(true);
 
   useEffect(() => {
     fetch("http://localhost:9292/games")
       .then((res) => res.json())
       .then((games) => {
         setGames(games);
-        console.log(games)
+        console.log(games);
       });
   }, []);
 
   function returnedUserId(id) {
-
     if (id) {
       setUserLoggedIn(true);
     } else {
       setUserLoggedIn(false);
     }
-
   }
-
 
   return (
     <div>
       <NavBar />
-      {userLoggedIn ?
+      {userLoggedIn ? (
         <Switch>
           <Route path="/GameReview/:id">
-
             <GameReview />
           </Route>
           <Route path="/">
             <Home games={games} />
           </Route>
         </Switch>
-
-        : <Login returnUserId={returnedUserId} />}
+      ) : (
+        <Login returnUserId={returnedUserId} />
+      )}
     </div>
   );
 };
