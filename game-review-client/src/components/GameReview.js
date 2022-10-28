58417f6033b722caabbd5e5d069c69c8d7b1c05d
Osamu Ito <mr.samito@gmail.com>
2022-10-28 13:17:32 -0400

    final project build w.rating functionality added

--- game-review-client/src/components/GameReview.js
+++ game-review-client/src/components/GameReview.js
@@ -1,14 +1,14 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import GameForm from "./GameForm";
-import { Card, Container } from "react-bootstrap";
+import Card from "react-bootstrap/Card";
 import uuid from "react-uuid";
-
 const GameReview = ({ currentUserId }) => {
   const [gameInfo, setGameInfo] = useState(null);
   const [gameReviews, setGameReviews] = useState([]);
+  const [reviewScore, setReviewScore] = useState(0.0);
+  const [reviewUpdate, setReviewUpdate] = useState(false);
   const { id } = useParams();
-
   useEffect(() => {
     fetch(`http://localhost:9292/games/${id}`)
       .then((res) => res.json())
@@ -17,25 +17,32 @@ const GameReview = ({ currentUserId }) => {
         setGameReviews(game.reviews);
       });
   }, []);
-
+  useEffect(() => {
+    fetch(`http://localhost:9292//gamesReview/${id}`)
+      .then((r) => r.json())
+      .then((score) => setReviewScore(score));
+  }, [reviewUpdate]);
+  function changeReview() {
+    setReviewUpdate(() => !reviewUpdate);
+  }
   // onAddReview passed down to Reviewform
   const onAddReview = (newReview) => {
     setGameReviews([...gameReviews, newReview]);
   };
-
   function handleDelete(e) {
     const filtered = gameReviews.filter(
       (review) => review.id !== parseInt(e.target.id)
     );
     fetch(`http://localhost:9292/reviews/${e.target.id}`, {
       method: "DELETE",
-    }).then(setGameReviews(filtered));
+    })
+      .then(setGameReviews(filtered))
+      .then(changeReview());
   }
-
   const displayReviews = gameReviews.map((review) => {
     return (
       <div key={uuid()}>
-        <label style={{ fontWeight: "bold" }}>{review.user.username}</label>
+        <label style={{ fontWeight: "bold" }}>{review.user.name}</label>
         <p style={{ fontWeight: "normal" }}>
           <strong>Score:</strong> {review.score}
           <br />
@@ -52,13 +59,60 @@ const GameReview = ({ currentUserId }) => {
       </div>
     );
   });
-
+  document.addEventListener("DOMContentLoaded", function () {
+    (function () {
+      let sr = document.querySelectorAll(".my-star");
+      let i = 0;
+      //loop through stars
+      while (i < sr.length) {
+        //attach click event
+        sr[i].addEventListener("click", function () {
+          //current star
+          let cs = parseInt(this.getAttribute("data-star"));
+          //output current clicked star value
+          document.querySelector("#output").value = cs;
+          /*our first loop to set the class on preceding star elements*/
+          let pre = cs; //set the current star value
+          //loop through and set the active class on preceding stars
+          while (1 <= pre) {
+            //check if the classlist contains the active class, if not, add the class
+            if (
+              !document
+                .querySelector(".star-" + pre)
+                .classList.contains("is-active")
+            ) {
+              document.querySelector(".star-" + pre).classList.add("is-active");
+            }
+            //decrement our current index
+            --pre;
+          } //end of first loop
+          /*our second loop to unset the class on succeeding star elements*/
+          //loop through and unset the active class, skipping the current star
+          let succ = cs + 1;
+          while (5 >= succ) {
+            //check if the classlist contains the active class, if yes, remove the class
+            if (
+              document
+                .querySelector(".star-" + succ)
+                .classList.contains("is-active")
+            ) {
+              document
+                .querySelector(".star-" + succ)
+                .classList.remove("is-active");
+            }
+            //increment current index
+            ++succ;
+          }
+        }); //end of click event
+        i++;
+      } //end of while loop
+    })(); //end of function
+  });
   if (gameInfo == null) {
     return <div></div>;
   }
-
   return (
-    <Container>
+    <div>
       <Card
         className="mx-aut"
         style={{
@@ -82,7 +136,7 @@ const GameReview = ({ currentUserId }) => {
           </Card.Title>
           <div>
             <p>
-              <strong>Description:</strong>
+              <strong>Description</strong>
               <br />
               {gameInfo.description}
             </p>
@@ -95,7 +149,7 @@ const GameReview = ({ currentUserId }) => {
             <p>
               <strong>Rating:</strong>
             </p>
-            <h4>--3 out of 5 stars--</h4>
+            <h4>--{reviewScore} out of 5 stars--</h4>
             <p>
               <strong>Reviews:</strong>
             </p>
@@ -103,14 +157,13 @@ const GameReview = ({ currentUserId }) => {
           </div>
         </Card.Body>
       </Card>
-
       <GameForm
         gameId={id}
         onAddReview={onAddReview}
         currentUserId={currentUserId}
+        changeReview={changeReview}
       />
-    </Container>
+    </div>
   );
 };
-
 export default GameReview;
--- game-review-server/app/controllers/application_controller.rb
+++ game-review-server/app/controllers/application_controller.rb
@@ -1,73 +1,75 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
-  
   # Add your routes here
-
   #Games route
   get "/games" do
     games = Game.all
     games.to_json(include: { reviews: { include: :user } })
   end
-  
-  # get '/games/:id/reviews' do
-  #     reviews = Game.find(params[:id]).reviews
-  #     reviews.to_json
-  # end
-
   #Users routes
   get '/users' do
     users = User.all
     users.to_json
   end
-
   get '/users/:id' do
     user = User.find(params[:id])
     user.to_json
   end
-
   post '/users' do
     newUser = User.new(params)
     newUser.save
     newUser.to_json
   end
-
   patch '/users/:id' do
     updateUser = User.find(params[:id])
     updateUser[:password] = params[:password]
     updateUser.save
     User.find(params[:id]).to_json
   end
-
   delete '/users/:id' do
     deleteUser = User.find(params[:id])
     deleteUser.reviews.destroy_all
     deleteUser.destroy
     deleteUser.save
   end
-
   get '/games/:id' do
     game = Game.find(params[:id])
     game.to_json(include: { reviews: { include: :user } })
   end
-
   #Reviews route
-
   get '/reviews' do
     reviews = Review.all
     reviews.to_json(only: [:id, :score, :comment, :game_id, :user_id, :created_at, :updated_at], include: {
       user: { only: [:id, :name, :lastname]}
     })
   end
-
   post '/reviews' do
     review = Review.new(params)
     review.save
     review.to_json(include: :user)
   end
-
   delete '/reviews/:id' do
     review = Review.find(params[:id])
     review.destroy
   end
-
+  get '/gamesReview/:id' do
+    sum = Game.find(params[:id]).reviews.sum{|r| r.score}
+    reviewCount = Game.find(params[:id]).reviews.count
+    a = reviewCount * 5
+    calculate = sum * 100 / a
+    f = calculate * 0.05
+    f.to_json
+  end
 end
+
+
+
+
+
+
+
+
+
+
+
+
